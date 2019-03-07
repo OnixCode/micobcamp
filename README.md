d673fbe1e7283c97e527b74825fbf69ab9c6f223
John Falzone <21jroc@gmail.com>
2019-03-07 16:59:01 -0600

    Changed ReadME heading

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# microbcamp
+# MicroTrain's Dev Boot Camp
 Working space for MicroTrain development bootcamp projects.
 First commit to dev.
--- bash/loop.sh
+++ bash/loop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
+# List all of the configuration files in the
+#_/etc/apache2/sites-available/_ directory
 VHOSTS_PATH=/etc/apache2/sites-available/*.conf 
 
 for FILENAME in ${VHOSTS_PATH}
